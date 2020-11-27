9327dee9ccadef86024fce0a2a1ad78a276ad3d4
刘正锟 <liuzhengkun@medlinker.com>
2020-11-27 16:56:06 +0800

    feat: 字符串编译完成

---
+++ bin/kpack.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+console.log("start");
+/**
+ * 1.拿到当前执行的路径，拿到配置文件
+ */
+
+let path = require("path");
+let Compiler = require("../lib/Compiler.js");
+let config = require(path.resolve("webpack.config.js"));
+
+let compiler = new Compiler(config);
+// 标识运行编译
+compiler.run()
