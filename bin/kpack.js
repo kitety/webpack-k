df65be85514c79e8c358a7fc9e7741f386d84615
刘正锟 <liuzhengkun@medlinker.com>
2020-11-27 17:59:19 +0800

    feat: 添加插件

--- bin/kpack.js
+++ bin/kpack.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-console.log("start");
+// console.log("start");
 /**
  * 1.拿到当前执行的路径，拿到配置文件
  */
@@ -10,5 +10,6 @@ let Compiler = require("../lib/Compiler.js");
 let config = require(path.resolve("webpack.config.js"));
 
 let compiler = new Compiler(config);
+compiler.hooks.entryOption.call();
 // 标识运行编译
-compiler.run()
+compiler.run();
